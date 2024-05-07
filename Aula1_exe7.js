50fd19481788fefe3abf3f1c636d95f9a37b5082
fpvill <144077908+fpvill@users.noreply.github.com>
2024-05-07 19:54:38 -0300

    Delete Aula1_exe7.js

--- Aula1_exe7.js
+++
@@ -1,7 +0,0 @@
-function inverterString(str) {
-    return str.split("").reverse().join("");
-}
-
-let stringOriginal = "Olá, mundo!";
-let stringInvertida = inverterString(stringOriginal);
-console.log(stringInvertida);
